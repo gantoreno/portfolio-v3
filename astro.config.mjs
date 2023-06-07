fa8e543ab7671f6573cb506bd8ce7db72c4d186b
Gabriel Moreno <gabriel.moreno@zemoga.com>
2023-06-07 13:57:06 -0400

    Integrate MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gantoreno.com",
-  integrations: [sitemap(), compress()],
+  integrations: [mdx(), sitemap(), compress()],
 });
