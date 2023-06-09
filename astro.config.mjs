300cdd8f572a7b43e298ff604e565099d9e1fdd3
Gabriel Moreno <gabriel.moreno@zemoga.com>
2023-06-09 13:44:12 -0400

    Use astro image for optimizing blog images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gantoreno.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [image(), mdx(), sitemap()],
 });
