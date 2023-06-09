8a882888547d33a734efa782c247345387ead241
Gabriel Moreno <gabriel.moreno@zemoga.com>
2023-06-09 14:04:18 -0400

    Simplify image structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gantoreno.com",
-  integrations: [image(), mdx(), sitemap()],
+  integrations: [mdx(), sitemap()],
 });
