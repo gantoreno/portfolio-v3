88ac1715cc88c8f704ae9f964c7dacf09a1b2635
Gabriel Moreno <gabriel.moreno@zemoga.com>
2023-07-09 20:11:50 -0400

    Update images with @astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from "astro/config";
 
+import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gantoreno.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
+    mdx(),
+    sitemap(),
+  ],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
