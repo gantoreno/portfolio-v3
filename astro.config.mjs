958babfc6e945f1e915be65809a659ecf877c5a1
Gabriel Moreno <gabriel.moreno@zemoga.com>
2023-07-09 20:18:12 -0400

    Revert "Update images with @astrojs/image"
    This reverts commit 88ac1715cc88c8f704ae9f964c7dacf09a1b2635.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
 import { defineConfig } from "astro/config";
 
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gantoreno.com",
-  integrations: [
-    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
