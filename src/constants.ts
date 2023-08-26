845268dadcb0cfdee1e11aff1662e9721107cb57
Gabriel Moreno <gabriel.moreno@zemoga.com>
2023-08-26 13:42:21 -0500

    Ditch previous style changes

--- src/constants.ts
+++ src/constants.ts
@@ -2,15 +2,3 @@ export const SITE_TITLE = "Gabriel Moreno - Welcome";
 export const SITE_DESCRIPTION = "Software engineer, full-stack web developer";
 export const SITE_KEYWORDS = ["Software Engineer", "Web Developer"];
 export const SITE_IMAGE = "/assets/img/banner.png";
-
-export const PAGE_IDENTIFIERS = {
-  HOME: "home",
-  BLOG: "blog",
-  NOT_FOUND: "not-found",
-};
-
-export const PAGE_EMOJIS = {
-  [PAGE_IDENTIFIERS.HOME]: "💡",
-  [PAGE_IDENTIFIERS.BLOG]: "🖋️",
-  [PAGE_IDENTIFIERS.NOT_FOUND]: "❓",
-};
